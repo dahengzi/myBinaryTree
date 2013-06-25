e5e41cc01e86394b7126ebbce608d79e7a77846b
Heng <dahengzi.ustc@gmail.com>
2013-06-25 19:15:21 +0800

    first commit

---
+++ myBinaryTree.h
@@ -0,0 +1,13 @@
+#include <stdio>
+#include <vector>
+#include <list>
+
+class myBinaryTreeNode
+{
+    public:
+    int Value;
+    myBinaryTreeNode* lchild;
+    myBinaryTreeNode* rchild;
+};
+
+typedef struct myBinaryTreeNode* myBTreeNodeP;
---
+++ myBinaryTreeTraverse.cpp
@@ -0,0 +1,14 @@
+#include "myBinaryTree.h"
+#include <vector>
+#include <list>
+#include <stack>
+
+void LeftChildTraverse(myBTreeNodeP node)
+{
+    stack<myBTreeNodeP> bstack;
+    myBTreeNodeP p = node;
+    while(p!=NULL)
+    {
+        bstack  
+    }
+}
